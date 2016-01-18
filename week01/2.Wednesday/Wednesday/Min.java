66d352ee38d1a12277e4b69c228336d5c13b0a28
= <ver.radenkova@gmail.com>
2016-01-18 21:02:14 +0200

    addComment

--- week01/2.Wednesday/Wednesday/Min.java
+++ week01/2.Wednesday/Wednesday/Min.java
@@ -1,16 +1,20 @@
 package week1;
+
 import java.util.Arrays;
 
 public class Min {
 
-	int min(int[] arr)
-	{
+	int min(int[] arr) {
 		Arrays.sort(arr);
 		return arr[0];
 	}
+
+	// mogesh da ne izpolzvash gotovi funkcii, a da izpolzvash for() s koito da
+	// obhogdash masiava i pri namirane na po-maluk el. ot min.el. da gi
+	// swap-nesh
 	public static void main(String[] args) {
-		Min num=new Min();
-		int[] array = new int[]{3,4,5,1,3,6,7};
+		Min num = new Min();
+		int[] array = new int[] { 3, 4, 5, 1, 3, 6, 7 };
 		System.out.println(num.min(array));
 
 	}
